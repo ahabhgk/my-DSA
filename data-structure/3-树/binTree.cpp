cfc9ae2a1de070f7bb77b202e945360b0e9040fd
ahabhgk <hi@ahabhgk.top>
2019-10-04 20:03:25 +0800

    add(tree): 二叉树

--- data-structure/3-树/binTree.cpp
+++ data-structure/3-树/binTree.cpp
@@ -2,66 +2,84 @@
 #include <vector>
 #include <functional>
 #include <queue>
+#include <string>
 using namespace std;
 
+template <typename T>
 struct Node {
-  char value;
+  T value;
   int left;
   int right;
 };
 
-const vector<Node> v1 = {
-  Node{'A', 1, 2},
-  Node{'B', 3, 4},
-  Node{'C', 5, -1},
-  Node{'D', -1, -1},
-  Node{'E', 6, -1},
-  Node{'G', 7, -1},
-  Node{'F', -1, -1},
-  Node{'H', -1, -1},
+const vector<Node<char> > v1 = {
+  Node<char>{'A', 1, 2},
+  Node<char>{'B', 3, 4},
+  Node<char>{'C', 5, -1},
+  Node<char>{'D', -1, -1},
+  Node<char>{'E', 6, -1},
+  Node<char>{'G', 7, -1},
+  Node<char>{'F', -1, -1},
+  Node<char>{'H', -1, -1},
 };
 
-const vector<Node> v2 = {
-  Node{'G', -1, 4},
-  Node{'B', 7, 6},
-  Node{'F', -1, -1},
-  Node{'A', 5, 1},
-  Node{'H', -1, -1},
-  Node{'C', 0, -1},
-  Node{'D', -1, -1},
-  Node{'E', 2, -1},
+const vector<Node<char> > v2 = {
+  Node<char>{'G', -1, 4},
+  Node<char>{'B', 7, 6},
+  Node<char>{'F', -1, -1},
+  Node<char>{'A', 5, 1},
+  Node<char>{'H', -1, -1},
+  Node<char>{'C', 0, -1},
+  Node<char>{'D', -1, -1},
+  Node<char>{'E', 2, -1},
 };
 
+const vector<Node<int> > v3 = {
+  Node<int>{30, 1, 2},
+  Node<int>{15, -1, -1},
+  Node<int>{41, 3, 4},
+  Node<int>{33, -1, -1},
+  Node<int>{50, -1, -1},
+};
+
+template <typename T>
 struct TreeNode {
-  char value;
+  T value;
   TreeNode* left;
   TreeNode* right;
 };
 
+template <class T>
 class BinTree {
-public:
-  BinTree(vector<Node> v) :TreeV(v.size()) {
-    int rootNum = 0;
-    for (int i = 0; i < v.size(); i++) {
-      TreeV[i] = TreeNode{v[i].value, nullptr, nullptr};
-      rootNum += i;
-    }
-    for (int i = 0; i < v.size(); i++) {
-      int leftNum = v[i].left;
-      int rightNum = v[i].right;
-      if (leftNum != -1) {
-        TreeV[i].left = &TreeV[leftNum];
-        rootNum -= leftNum;
-      }
-      if (rightNum != -1) {
-        TreeV[i].right = &TreeV[rightNum];
-        rootNum -= rightNum;
-      }
-    }
-    root = &TreeV[rootNum];
-  }
+private:
+  TreeNode<T>* root;
+  // vector<TreeNode<T> > TreeV;
 
-  void inOrderTraversal(TreeNode* root, function<void (char)> const &f) {
+public:
+  BinTree() :root(nullptr) {};
+  ~BinTree() { clear(); };
+  // BinTree(vector<Node<T> > v) :TreeV(v.size() * 2) {
+  //   int rootNum = 0;
+  //   for (int i = 0; i < v.size(); i++) {
+  //     TreeV[i] = TreeNode<T>{v[i].value, nullptr, nullptr};
+  //     rootNum += i;
+  //   }
+  //   for (int i = 0; i < v.size(); i++) {
+  //     int leftNum = v[i].left;
+  //     int rightNum = v[i].right;
+  //     if (leftNum != -1) {
+  //       TreeV[i].left = &TreeV[leftNum];
+  //       rootNum -= leftNum;
+  //     }
+  //     if (rightNum != -1) {
+  //       TreeV[i].right = &TreeV[rightNum];
+  //       rootNum -= rightNum;
+  //     }
+  //   }
+  //   root = &TreeV[rootNum];
+  // }
+
+  void inOrderTraversal(TreeNode<T>* root, function<void (T)> const &f) {
     if (root != nullptr) {
       inOrderTraversal(root->left, f);
       f(root->value);
@@ -69,11 +87,11 @@ public:
     }
   }
 
-  void inOrderTraversal(function<void (char)> const &f) {
+  void inOrderTraversal(function<void (T)> const &f) {
     inOrderTraversal(root, f);
   }
 
-  void preOrderTraversal(TreeNode* root, function<void (char)> const &f) {
+  void preOrderTraversal(TreeNode<T>* root, function<void (T)> const &f) {
     if (root != nullptr) {
       f(root->value);
       preOrderTraversal(root->left, f);
@@ -81,11 +99,11 @@ public:
     }
   }
 
-  void preOrderTraversal(function<void (char)> const &f) {
+  void preOrderTraversal(function<void (T)> const &f) {
     preOrderTraversal(root, f);
   }
 
-  void postOrderTraversal(TreeNode* root, function<void (char)> const &f) {
+  void postOrderTraversal(TreeNode<T>* root, function<void (T)> const &f) {
     if (root != nullptr) {
       postOrderTraversal(root->left, f);
       postOrderTraversal(root->right, f);
@@ -93,18 +111,18 @@ public:
     }
   }
 
-  void postOrderTraversal(function<void (char)> const &f) {
+  void postOrderTraversal(function<void (T)> const &f) {
     postOrderTraversal(root, f);
   }
 
-  void levelOrderTraversal(TreeNode* root, function<void (char)> const &f) {
-    queue<TreeNode> q;
+  void levelOrderTraversal(TreeNode<T>* root, function<void (T)> const &f) {
+    queue<TreeNode<T> > q;
 
     if (root == nullptr) return;
     q.push(*root);
 
     while (!q.empty()) {
-      TreeNode* t = &q.front();
+      TreeNode<T>* t = &q.front();
       q.pop();
       f(t->value);
       if (t->left != nullptr) q.push(*t->left);
@@ -112,11 +130,11 @@ public:
     }
   }
 
-  void levelOrderTraversal(function<void (char)> const &f) {
+  void levelOrderTraversal(function<void (T)> const &f) {
     levelOrderTraversal(root, f);
   }
 
-  int height(TreeNode* root) {
+  int height(TreeNode<T>* root) {
     if (root == nullptr) return 0;
     int lh = height(root->left);
     int lr = height(root->right);
@@ -127,25 +145,195 @@ public:
     return height(root);
   }
 
-private:
-  TreeNode* root;
-  vector<TreeNode> TreeV;
+  TreeNode<T>* find(TreeNode<T>* root, T val) {
+    if (val == root->value) return root;
+    else if (val < root->value) return find(root->left, val);
+    else if (val > root->value) return find(root->right, val);
+    else return nullptr;
+  }
+
+  TreeNode<T>* find(T val) {
+    return find(root, val);
+  }
+
+  TreeNode<T>* iterFind(TreeNode<T>* root, T val) {
+    while (root != nullptr) {
+      if (val == root->value) return root;
+      else if (val < root->value) root = root->left;
+      else if (val > root->value) root = root->right;
+    }
+    return nullptr;
+  }
+
+  TreeNode<T>* iterFind(T val) {
+    return iterFind(root, val);
+  }
+
+  TreeNode<T>* findMin(TreeNode<T>* root) {
+    if (root->left == nullptr) return root;
+    else return findMin(root->left);
+  }
+
+  TreeNode<T>* findMin() {
+    return findMin(root);
+  }
+
+  TreeNode<T>* findMax(TreeNode<T>* root) {
+    if (root->right == nullptr) return root;
+    else return findMax(root->right);
+  }
+
+  TreeNode<T>* findMax() {
+    return findMax(root);
+  }
+
+  TreeNode<T>* iterFindMin(TreeNode<T>* root) {
+    while (root->left != nullptr) {
+      root = root->left;
+    }
+    return root;
+  }
+
+  TreeNode<T>* iterFindMin() {
+    return iterFindMin(root);
+  }
+
+  TreeNode<T>* iterFindMax(TreeNode<T>* root) {
+    while (root->right != nullptr) {
+      root = root->right;
+    }
+    return root;
+  }
+
+  TreeNode<T>* iterFindMax() {
+    return iterFindMax(root);
+  }
+
+  void insert(TreeNode<T>*& root, T val) {
+    if (root == nullptr) {
+      TreeNode<T>* node = new TreeNode<T>{val, nullptr, nullptr};
+      root = node;
+    }
+    else if (val < root->value) insert(root->left, val);
+    else if (val > root->value) insert(root->right, val);
+  }
+
+  BinTree& insert(T val) {
+    insert(root, val);
+    return *this;
+  }
+
+  void del(TreeNode<T>*& root, T val) {
+    if (root == nullptr) cout << "The element to be deleted was not found..." << endl;
+    else if (val == root->value) {
+      if (root->left != nullptr && root->right != nullptr) {
+        // 取右子树中最小的元素替代 or 取左子树中最大的元素替代
+        TreeNode<T>* rightMin = findMin(root->right);
+        root->value = rightMin->value;
+        del(root->right, root->value);
+      } else {
+        TreeNode<T>* tmp = root;
+        if (!root->left) root = root->right;
+        else if (!root->right) root = root->left;
+        delete tmp;
+      }
+    }
+    else if (val < root->value) del(root->left, val);
+    else if (val > root->value) del(root->right, val);
+  }
+
+  BinTree& del(T val) {
+    del(root, val);
+    return *this;
+  }
+
+  void clear(TreeNode<T>*& root) {
+    if (root == nullptr) return;
+    clear(root->left);
+    clear(root->right);
+    delete root;
+    root = nullptr;
+  }
+
+  BinTree& clear() {
+    clear(root);
+    return *this;
+  }
 };
 
 int main() {
-  BinTree tree(v1);
+  // BinTree tree(v2);
 
-  cout << "preOrderTraversal: " << endl;
-  tree.preOrderTraversal([](char c) { cout << c << endl; });
+  // cout << "preOrderTraversal: " << endl;
+  // tree.preOrderTraversal([](auto c) { cout << c << endl; });
 
-  cout << "inOrderTraversal: " << endl;
-  tree.inOrderTraversal([](char c) { cout << c << endl; });
+  // cout << "inOrderTraversal: " << endl;
+  // tree.inOrderTraversal([](auto c) { cout << c << endl; });
 
-  cout << "postOrderTraversal: " << endl;
-  tree.postOrderTraversal([](char c) { cout << c << endl; });
+  // cout << "postOrderTraversal: " << endl;
+  // tree.postOrderTraversal([](auto c) { cout << c << endl; });
 
-  cout << "levelOrderTraversal: " << endl;
-  tree.levelOrderTraversal([](char c) { cout << c << endl; });
+  // cout << "levelOrderTraversal: " << endl;
+  // tree.levelOrderTraversal([](auto c) { cout << c << endl; });
 
-  cout << "height: " << tree.height() << endl;
+  // cout << "height: " << tree.height() << endl;
+
+  // BinTree searchTree(v3);
+
+  // cout << "preOrderTraversal: " << endl;
+  // searchTree.preOrderTraversal([](auto c) { cout << c << endl; });
+
+  // cout << "inOrderTraversal: " << endl;
+  // searchTree.inOrderTraversal([](auto c) { cout << c << endl; });
+
+  // cout << "postOrderTraversal: " << endl;
+  // searchTree.postOrderTraversal([](auto c) { cout << c << endl; });
+
+  // cout << "levelOrderTraversal: " << endl;
+  // searchTree.levelOrderTraversal([](auto c) { cout << c << endl; });
+
+  // cout << "height: " << searchTree.height() << endl;
+
+  // TreeNode<int>* val30Node = searchTree.find(30);
+  // cout << "find 30 &: " << val30Node << endl;
+  // cout << "find 30 value: " << val30Node->value << endl;
+
+  // TreeNode<int>* val15Node = searchTree.iterFind(15);
+  // cout << "iterFind 15 &: " << val15Node << endl;
+  // cout << "iterFind 15 value: " << val15Node->value << endl;
+
+  // TreeNode<int>* maxNode = searchTree.findMax();
+  // cout << "find max &: " << maxNode << endl;
+  // cout << "find max value: " << maxNode->value << endl;
+
+  // TreeNode<int>* minNode = searchTree.iterFindMin();
+  // cout << "find min &: " << minNode << endl;
+  // cout << "find min value: " << minNode->value << endl;
+
+  BinTree<string> st;
+  st
+    .insert("Jan")
+    .insert("Feb")
+    .insert("Mar")
+    .insert("Apr")
+    .insert("May")
+    .insert("Jun")
+    .insert("July")
+    .insert("Aug")
+    .insert("Sep")
+    .insert("Oct")
+    .insert("Nov")
+    .insert("Dec")
+    .del("Mar");
+  //        Jan                      Jan
+  //    Fab    Mar(X)            Fab     May
+  // Apr     Jun May     =>    Apr     Jun   Sep
+  //  Aug  July    Sep          Aug  July  Oct
+  //   Dec        Oct            Dec      Nov
+  //             Nov
+  st.levelOrderTraversal([](auto c) { cout << c << endl; });
+  cout << st.height() << endl;
+  
+
+  // BinTree st();
 }
